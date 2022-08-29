645cac44057ab55a9dab396fbf0353af8c9125e6
umermasood <umermasood.dev@gmail.com>
2022-08-29 12:08:35 +0500

    Implemented Subtests with refactoring

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris!"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("saying hello world when empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Got %q, want %q", got, want)
 	}
