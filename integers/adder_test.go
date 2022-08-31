c2f878407213485da465cfddcab13fdf6e895dea
umermasood <umermasood.dev@gmail.com>
2022-08-31 14:33:08 +0500

    Added Integers Implementation

---
+++ integers/adder_test.go
@@ -0,0 +1,28 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(2, 2)
+	fmt.Println(sum)
+	// Output: 4
+}
+
+func ExampleAdd_second() {
+	sum := Add(2, 6)
+	fmt.Println(sum)
+	// Output:
+	// 8
+}
