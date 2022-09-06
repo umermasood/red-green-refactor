fa992a316cdbe1eb90966afbe2e839d5aa2180a0
umermasood <umermasood.dev@gmail.com>
2022-09-06 23:36:42 +0500

    Handled unchecked errors

--- pointersanderrors/wallet_test.go
+++ pointersanderrors/wallet_test.go
@@ -7,12 +7,15 @@ func TestWallet(t *testing.T) {
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
-		_ = wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -30,17 +33,24 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	got := wallet.Balance()
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
 
-func assertError(t testing.TB, got, want error) {
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
 	t.Helper()
 	if got == nil {
 		t.Fatal("didn't get an error but wanted one")
 	}
 
 	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+		t.Errorf("got %s, want %s", got, want)
 	}
 }
