93f69054188c5c7183de8098029e6888409d3a21
umermasood <umermasood.dev@gmail.com>
2022-09-11 15:13:40 +0500

    Implemented mock testing of countdown example

---
+++ mocking/countdown.go
@@ -0,0 +1,52 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func Countdown(writer io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		_, _ = fmt.Fprintln(writer, i)
+		sleeper.Sleep()
+	}
+	_, _ = fmt.Fprintln(writer, finalWord)
+}
---
+++ mocking/countdown_test.go
@@ -0,0 +1,46 @@
+package mocking
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	t.Run("print 3 2 1 Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := "3\n2\n1\nGo!\n"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+		if spySleeper.Calls != 3 {
+			t.Errorf("not enough calls to the sleeper, want 3 calls, got %q calls", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every second prints", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(spySleepPrinter.Calls, want) {
+			t.Errorf("wanted calls %v, got calls %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
